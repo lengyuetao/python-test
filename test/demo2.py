a54c8e584fd5d9cc405490bf7d2498bf65950728
lengyuetao <dyadan@163.com>
2018-10-10 17:31:36 +0800

    函数

--- test/demo2.py
+++ test/demo2.py
@@ -11,3 +11,24 @@ def area(width,height):
 
 
 print(area(10,10));
+
+
+def console():
+    print("控制台输出。。。。")
+    hello();
+
+
+console();
+
+
+def show(name,age):
+    print("名字：",name);
+    print("年龄：",age);
+
+
+# 不要指定参数顺序
+show(age=123, name="李四");
+
+
+# 要指定参数顺序
+show("刘备", 56);
