e65607cbbd2bfde500981619e8743778754f1f48
lengyuetao <dyadan@163.com>
2018-10-10 16:22:18 +0800

    条件，循环

--- test/demo1.py
+++ test/demo1.py
@@ -6,3 +6,10 @@ for i in range(0, 10, 3):
 address = ['北京', '上海', '深圳'];
 for i in range(len(address)):
     print(address[i]);
+
+# 迭代器遍历列表
+lists = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+it = iter(lists);
+for i in it:
+    print(i);
