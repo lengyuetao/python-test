9036d4f93757b75c2bb1e28f18595fbe59f50c19
lengyuetao <dyadan@163.com>
2018-10-10 15:08:44 +0800

    条件，循环

--- test/demo.py
+++ test/demo.py
@@ -1,6 +1,15 @@
+# 循环语句
 a, b = 0, 1
 
 while b < 10:
     # end可以将结果输出到同一行
     print(b,end=",")
     a, b = b, a+b
+
+# 条件语句 条件如果为0则为false
+number = 1;
+
+if number:
+    print("*******");
+else:
+    print("看看看看");
