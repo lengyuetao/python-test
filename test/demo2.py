3ab21ed8c398d8a6fd94ec9eb6286fd1224d6b81
lengyuetao <dyadan@163.com>
2018-10-10 17:47:52 +0800

    函数

--- test/demo2.py
+++ test/demo2.py
@@ -32,3 +32,27 @@ show(age=123, name="李四");
 
 # 要指定参数顺序
 show("刘备", 56);
+
+
+# 星号表示可以传入多个参数，类型是元组,如果为空则为空元组
+def one(arg1,*var):
+    print(var);
+
+
+one(12, 2, 3, 23);
+
+
+# 以字典形式传入
+def two(arg1,**var):
+    print(var);
+
+
+two(12,a=23,b=234,c=2345,d=23456);
+
+
+# 如果参数单独出现在星号后面，param=value参数形式传入
+def three(a,b,*,c):
+    print(c);
+
+
+three(23,23,c=10292)
